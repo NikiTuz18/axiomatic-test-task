d728ddf7f76a93ee9247399fa4b8362a8ad32e05
NikiTuz18 <nevenchanniy.egor.90@gmail.com>
2023-10-27 14:37:30 +0300

    init application

---
+++ src/main/java/ru/nikituz/axiomatictesttask/AxiomaticTestTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.nikituz.axiomatictesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxiomaticTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxiomaticTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/nikituz/axiomatictesttask/AxiomaticTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nikituz.axiomatictesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxiomaticTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
