bb24b99b0155d254e62a2706ce11099fdacdb61c
NikiTuz18 <nevenchanniy.egor.90@gmail.com>
2023-10-27 16:19:44 +0300

    Configure Hibernate

---
+++ src/main/java/ru/nikituz/axiomatictesttask/configurations/hibernate/HibernateConfiguration.java
@@ -0,0 +1,61 @@
+package ru.nikituz.axiomatictesttask.configurations.hibernate;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import ru.nikituz.axiomatictesttask.configurations.hibernate.properties.DataSourceProperties;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan(basePackages = "ru.nikituz.axiomatictesttask")
+@PropertySource("classpath:application.yaml")
+@RequiredArgsConstructor
+public class HibernateConfiguration {
+
+    private final DataSourceProperties dataSourceProperties;
+
+    @Bean
+    public PlatformTransactionManager hibernateTransactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(this.sessionFactory().getObject());
+        return transactionManager;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        final LocalSessionFactoryBean factory = new LocalSessionFactoryBean();
+        factory.setDataSource(this.dataSource());
+        factory.setPackagesToScan("ru.nikituz.axiotestpractice");
+        factory.setHibernateProperties(this.hibernateProperties());
+        return factory;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(this.dataSourceProperties.getDriverClassName());
+        dataSource.setUrl(this.dataSourceProperties.getUrl());
+        dataSource.setUsername(this.dataSourceProperties.getUsername());
+        dataSource.setPassword(this.dataSourceProperties.getPassword());
+        return dataSource;
+    }
+
+    @Bean
+    Properties hibernateProperties(){
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        properties.setProperty("current_session_context_class", "thread");
+        return properties;
+    }
+}
